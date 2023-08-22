aff5d3d07c2f13efdac89d96b94a852489d4740c
HenriMalahieude <henrimalahieude@gmail.com>
2023-08-22 10:51:01 -0700

    Decided to do Manual 50 Epoch Tests

--- DifferentModels/Manual_Shots/AlexNet.py
+++ DifferentModels/Manual_Shots/AlexNet.py
@@ -48,31 +48,6 @@ print("\nAugmenting Data")
 train_data = base_train.concatenate(train_ds.map(preprocess2))
 print("\tTraining Data increased to:", str(train_data.cardinality() * BATCH_SIZE))
 
-
-for train_x, train_y in train_data:
-    print("\tTrain Shape is TX:", train_x.shape, "TY:", train_y.shape)
-    #print(train_y)
-    break
-"""
-min_x, min_y = 10000, 10000
-max_x, max_y = 0, 0
-Batch_size = 0
-Color_size = 3
-for x_train, y_train in train_data:
-    B, X, Y, C = x_train.shape
-    Batch_size = B
-    Color_size = C
-    if min_x * min_y > X * Y:
-        min_x = X
-        min_y = Y
-    elif X * Y > max_x * max_y:
-        max_x = X
-        max_y = Y
-
-print("Maximum seen: ", max_x, " x ", max_y)
-print("Minimum seen: ", min_x, " x ", min_y)
-print(Batch_size, max_x, max_y, Color_size)
-#"""
 print("\nCreating Mirrored Multi-GPU Strategy")
 strat = tf.distribute.MirroredStrategy()
 print("\tNumber of Devices: {}".format(strat.num_replicas_in_sync))
@@ -82,50 +57,54 @@ print("\tNumber of Devices: {}".format(strat.num_replicas_in_sync))
 with strat.scope():
     alex_net = tf.keras.models.Sequential()
     alex_net.add(tf.keras.layers.Conv2D(96, 11, strides=(4, 4), input_shape=IMAGE_SIZE))
-    alex_net.add(tf.keras.layers.Activation('relu'))
-    alex_net.add(tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(2,2)))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
+    alex_net.add(tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(2, 2)))
 
     alex_net.add(tf.keras.layers.Conv2D(256, 5, padding='same'))
-    alex_net.add(tf.keras.layers.Activation('relu'))
-    alex_net.add(tf.keras.layers.AveragePooling2D(pool_size=(3,3), strides=(2,2)))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
+    alex_net.add(tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(2, 2)))
 
     alex_net.add(tf.keras.layers.Conv2D(384, 3, padding='same'))
-    alex_net.add(tf.keras.layers.Activation('relu'))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
     alex_net.add(tf.keras.layers.Conv2D(384, 3, padding='same'))
-    alex_net.add(tf.keras.layers.Activation('relu'))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
     alex_net.add(tf.keras.layers.Conv2D(256, 3, padding='same'))
-    alex_net.add(tf.keras.layers.Activation('relu'))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
     alex_net.add(tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(2,2)))
     alex_net.add(tf.keras.layers.Flatten())
 
     alex_net.add(tf.keras.layers.Dense(4096))
-    alex_net.add(tf.keras.layers.Activation('relu'))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
     alex_net.add(tf.keras.layers.Dropout(DROPOUT_RATE))
 
     alex_net.add(tf.keras.layers.Dense(4096))
-    alex_net.add(tf.keras.layers.Activation('relu'))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
     alex_net.add(tf.keras.layers.Dropout(DROPOUT_RATE))
 
     alex_net.add(tf.keras.layers.Dense(1000))
-    alex_net.add(tf.keras.layers.Activation('relu'))
+    alex_net.add(II.ActivationLinearizer())#tf.keras.layers.Activation('relu'))
 
     alex_net.add(tf.keras.layers.Dense(10)) #Number of Classes in our thing
     alex_net.add(tf.keras.layers.Activation('softmax'))
 
-    metrics_to_use = [tf.keras.metrics.TopKCategoricalAccuracy(name='T5'), tf.keras.metrics.TopKCategoricalAccuracy(name='T3', k=3), tf.keras.metrics.TopKCategoricalAccuracy(name='T2', k=2)]
+    metrics_to_use = [
+        tf.keras.metrics.TopKCategoricalAccuracy(name='T5'), 
+        tf.keras.metrics.TopKCategoricalAccuracy(name='T3', k=3), 
+        tf.keras.metrics.TopKCategoricalAccuracy(name='T1', k=1)
+    ]
 
 
 #alex_net.summary(line_length=100)
 
 print("\nTraining Model w/ {} of Dropout".format(DROPOUT_RATE))
 alex_net.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=metrics_to_use)
-alex_net.fit(x=train_data, epochs=5, validation_data=val_data)
+alex_net.fit(x=train_data, epochs=50, validation_data=val_data)
 
 print("\n")
 
-then = time.time()
-alex_net.evaluate(val_data)
-print("Took: ", str(int((time.time() - then) * 1000)) + "ms")
+#then = time.time()
+#alex_net.evaluate(val_data)
+#print("Took: ", str(int((time.time() - then) * 1000)) + "ms")
 
 #Basic AlexNet (T5 Acc) -> 3.306s for 95-100% final validation accuracy (Training is ~50-60% top-5 acc)
 #---
@@ -150,11 +129,11 @@ print("Took: ", str(int((time.time() - then) * 1000)) + "ms")
 #Control ->  5.075s for 100% FVT5A,   0% FVT32A , but 100% T3A when trained other times (Training is T5 ~95-93%   , T3 ~48-63-61%, T2 ~29-42%   )
 
 
-#3PLUs   ->  4.980s for 100% FVT2A,   -         , has been 100% T2A since Epoch 2       (Training is T5 ~53-59%   , T3 ~32-30-35%, T2 ~17-25-23%)
+#3PLUs   ->  4.980s for 100% FVT2A,  -          , has been 100% T2A since Epoch 2       (Training is T5 ~53-59%   , T3 ~32-30-35%, T2 ~17-25-23%)
 #. . .
 #6PLUs   ->  5.181s for  47% FVT5A, 0% FVT32A   , but 97% T5 and 64% T3 at Epoch 3      (Training is T5 ~57-70-66%, T3 ~31-50%   , T2 ~18-34%   )
 #. . .
-#8PLUs   ->  6.000s for 100% FVT2A,   -         , was 100% T2A at Epoch 2 only          (Training is T5 ~52-55%   , T3 ~32-37-32%, T2 ~19-24-20%)
+#8PLUs   ->  6.000s for 100% FVT2A,  -          , was 100% T2A at Epoch 2 only          (Training is T5 ~52-55%   , T3 ~32-37-32%, T2 ~19-24-20%)
 
 
 #3ALs    ->  5.000s for 100% FVT3A, 0% FVT2A    , was 100% T3A Epoch 5 only             (Training is T5 ~50-77-67%, T3 ~31-50-38%, T2 ~23-35-24%)
@@ -162,3 +141,11 @@ print("Took: ", str(int((time.time() - then) * 1000)) + "ms")
 #6ALs    ->  5.023s for 100% FVT2A,  -          , had 100% T3A & 98% T2A Epoch 1 only   (Training is T5 ~60-90%   , T3 ~38-67&   , T2 ~25-47%   )
 #. . .
 #8ALs    ->  5.117s for   0% FVT532A,-          , but 100% T5A Epochs 1, 2 & 3          (Training is T5 ~66-84%   , T3 ~34-65%   , T2 ~19-50%   )
+
+#------------------------------------------Long Test of 50 Epochs
+#NOTE: For this control, T1 is actually T2 v
+#            (T5) (T3) (T1)   (TL)    (V5) (V3) (V1)   (VL)     (BE) (V5) (V3) (V1)   (VL)
+#Control ->  100% 100% 090% - 2.302 | 100% 100% 100% - 2.304 || (13) 100% 100% 100% - 2.304 (NOTE: All Top 1 accuracies were 0% on second test)
+
+#All PLUs->  088% 050% 006% - 2.302 | 100% 000% 000% - 2.304 || (13) 100% 100% 099% - 2.303
+#All ALs ->  100% 100% 000% - 2.302 | 100% 100% 000% - 2.304 || (11) 100% 100% 000% - 2.305
