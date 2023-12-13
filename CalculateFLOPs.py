8de3b53fe4320cddeaac539ea17070d02e768054
HenriMalahieude <henrimalahieude@gmail.com>
2023-12-12 16:27:44 -0800

    New MobileNeXt testing bench

--- CalculateFLOPs.py
+++ CalculateFLOPs.py
@@ -4,103 +4,13 @@ import InferredActivations.Inferrer as II
 from tensorflow.python.profiler import model_analyzer, option_builder
 from keras import layers, models
 
-class ShuffleUnit(layers.Layer):
-	def __init__(self, out_channels, strides=2, stage=1):
-		super(ShuffleUnit, self).__init__()
-		self.out_channels = out_channels
-		self.strides = strides
-		self.stage = stage
-
-	def build(self, input_shape): #NOTE: Expecting Channels Last
-		bottleneck_channels = int(self.out_channels * 0.5)
-
-		self.right = models.Sequential([
-			layers.Conv2D(bottleneck_channels, 1, strides=1, padding='same'),
-			layers.BatchNormalization(-1),
-			layers.Activation('relu'),
-			layers.DepthwiseConv2D(kernel_size=3, strides=self.strides, padding='same'),
-			layers.BatchNormalization(-1),
-			layers.Conv2D(bottleneck_channels, 1, strides=1, padding='same'),
-			layers.BatchNormalization(-1),
-			layers.Activation('relu')
-		])
-
-		if self.strides >= 2:
-			self.left_on_stride2 = models.Sequential([
-				layers.DepthwiseConv2D(kernel_size=3, strides=2, padding='same'),
-				layers.BatchNormalization(-1),
-				layers.Conv2D(bottleneck_channels, 1, strides=1, padding='same'),
-				layers.BatchNormalization(-1),
-				layers.Activation('relu')
-			])
-
-	#Expects channels last, returns c_hat, c
-	def channel_split(self, input):
-		channels = input.shape.as_list()[-1]
-		cp_split = channels // 2
-		return input[:, :, :, 0:cp_split], input[:, :, :, cp_split:]
-	
-	def channel_shuffle(self, input):
-		height, width, channels = input.shape.as_list()[-3:]
-		cp_split = channels // 2
-		x = layers.Reshape([height, width, 2, cp_split])(input)
-		x = layers.Permute((1,2,4,3))(x)
-		return layers.Reshape([height, width, channels])(x)
-
-
-
-	def call(self, input):
-		if self.strides < 2:
-			c_hat, c = self.channel_split(input)
-			input = c
-
-		r1 = self.right(input)
-
-		if self.strides < 2:
-			ret = layers.Concatenate(-1)([r1, c_hat])
-		else:
-			l1 = self.left_on_stride2(input)
-			ret = layers.Concatenate(-1)([r1, l1])
-
-		return self.channel_shuffle(ret)
-
-class ShuffleBlock(layers.Layer):
-	def __init__(self, channel_map, repeat=1, stage=1):
-		self.channel_map = channel_map
-		self.repeat = repeat
-		self.stage = stage
-		super(ShuffleBlock, self).__init__()
-
-	def build(self, input_shape):
-		self.units = models.Sequential([
-			ShuffleUnit(self.channel_map[self.stage-1], strides=2, stage=self.stage), # +3
-			*[ShuffleUnit(self.channel_map[self.stage-1], strides=1, stage=self.stage) for _ in range(1, self.repeat+1)] # +2 * repeat
-		])
-
-		self.units.build(input_shape)
-
-	def call(self, input):
-		return self.units(input)
-
-out_channels_in_stage = 2 ** np.insert(np.arange(len([3, 7, 3]), dtype=np.float32), 0, 0) #ie [1, 1, 2, 4]
-out_channels_in_stage *= 48
-out_channels_in_stage[0] = 24 #according to the github first stage always has 24 output channels
-out_channels_in_stage = out_channels_in_stage.astype(int)
+act_to_use = layers.Activation
+act_arg1 = "relu"
+act_arg2 = "relu6"
 
 model = models.Sequential([
-    layers.Conv2D(24, 3, strides=2, padding='same', use_bias=False, input_shape=(224, 224, 3)),
-    layers.Activation('relu'),
-    layers.MaxPool2D(3, 2),
-
-    #ShuffleBlock(out_channels_in_stage, repeat=SHUFFLE_BLOCKS[0], stage=2),
-    *[ShuffleBlock(out_channels_in_stage, repeat=[3, 7, 3][i], stage=i+2) for i in range(len([3, 7, 3]))],
-    
-    layers.Conv2D(1024, 1, strides=1, padding='same'),
-    layers.Activation('relu'),
-    layers.GlobalMaxPooling2D(),
-    layers.Dense(10, activation="softmax"),
+	
 ])
-
 
 #Don't change what's below!
 #------------------------------------------------------------------------------------------------------------------------------
---
+++ MobileNeXt_Big.py
@@ -0,0 +1,151 @@
+import tensorflow as tf
+import InferredActivations.Inferrer as II
+import Helpers as h
+from keras import layers, models
+
+logger = h.create_logger("mobilenext_dump.log")
+
+#Man, if only there was a way to not have to copy and paste this.....
+TYPE = "control"
+assert TYPE in ["control", "pwlu", "al"]
+AUGMENT_FACTOR = 0.05
+CONCAT_AUG = True
+
+BATCH_SIZE = 128 if TYPE == "control" else (64 if TYPE == "al" else 32)
+EPOCHS = 15
+INIT_LRATE = 0.05
+#LRATE_SCHED = 5
+#LRATE_RATIO = 0.1
+
+W_DECAY = 0.00004
+MOMENTUM = 0.9
+
+IMAGE_SIZE = (225, 225)
+
+print((
+	'Beginning mobile neXt testing!'
+	f'\n\t{TYPE} type'
+	#f'\n\t{BOTTLENECK_RATIO} bottleneck ratio'
+	#f'\n\t{SHUFFLE_BLOCKS} shuffle blocks'
+	#f'\n\t{SCALE_FACTOR} scale factor'
+	f'\n\n\t{BATCH_SIZE} batch size'
+	f'\n\t{AUGMENT_FACTOR} augment factor'
+	#f'\n\t{DROPOUT} of dropout'
+	f'\n\tConcatenate Augment? {CONCAT_AUG}'
+	f'\n\n\t{EPOCHS} total epochs'
+	f'\n\t{INIT_LRATE} initial learning rate'
+	f'\n\tLearning rate schedule of cosine ratio every -- epochs'
+	f'\n\t{W_DECAY} weight decay'
+	f'\n\t{MOMENTUM} SGD momentum'
+))
+
+act_to_use = layers.Activation if TYPE == 'control' else (II.ActivationLinearizer if TYPE == "al" else II.PiecewiseLinearUnitV1)
+act_arg1 = "relu" if TYPE != "pwlu" else 20
+act_arg2 = "relu6" if TYPE != "pwlu" else 10
+
+print("\nPrepping CIFAR-10 Dataset")
+train_ds, val_ds = h.load_cifar10(BATCH_SIZE)
+
+train_ds = h.prepare_dataset(train_ds, IMAGE_SIZE, augment_factor=AUGMENT_FACTOR, concatenate_augment=CONCAT_AUG)
+h.report_dataset_size("Training", train_ds, BATCH_SIZE)
+val_ds = h.prepare_dataset(val_ds, IMAGE_SIZE)
+h.report_dataset_size("Validation", val_ds, BATCH_SIZE)
+
+print("\nDefining Sandglass Block")
+class SandglassBlock(layers.Layer):
+	def __init__(self, reduction_ratio, out_channels, stride=1):
+		super(SandglassBlock, self).__init__()
+		self.t = reduction_ratio
+		self.n = out_channels
+		self.s = stride
+
+	def build(self, input_shape):
+		m = input_shape[-1]
+
+		self.sequence = models.Sequential([
+			layers.DepthwiseConv2D(3, padding='same'),
+			layers.BatchNormalization(-1),
+			act_to_use(act_arg2),
+
+			layers.Conv2D(m // self.t, 1),
+			layers.BatchNormalization(-1),
+
+			layers.Conv2D(self.n, 1),
+			layers.BatchNormalization(-1),
+			act_to_use(act_arg2),
+
+			layers.DepthwiseConv2D(3, self.s, padding='same'),
+			layers.BatchNormalization(-1),
+		])
+
+	def call(self, input):
+		s1 = self.sequence(input)
+
+		if input.shape.as_list() == s1.shape.as_list():
+			return input + s1
+		
+		return s1
+	
+print("\nDefining Model")
+strat = tf.distribute.MirroredStrategy()
+print(f"\tUsing {strat.num_replicas_in_sync} GPUs")
+
+with strat.scope():
+	optim = tf.keras.optimizers.experimental.SGD(tf.keras.optimizers.schedules.CosineDecay(INIT_LRATE, 50000), MOMENTUM, weight_decay=(W_DECAY if W_DECAY > 0 else None))
+
+	neXt = models.Sequential([
+		layers.Conv2D(32, 3, strides=2, input_shape=(*IMAGE_SIZE, 3)),
+		layers.BatchNormalization(-1),
+		act_to_use(act_arg1),
+		
+		SandglassBlock(2, 96, 2),
+
+		SandglassBlock(6, 144),
+
+		SandglassBlock(6, 192, 2),
+		SandglassBlock(6, 192),
+		SandglassBlock(6, 192),
+
+		SandglassBlock(6, 288, 2),
+		SandglassBlock(6, 288),
+		SandglassBlock(6, 288),
+
+		*[SandglassBlock(6, 384) for _ in range(4)],
+
+		SandglassBlock(6, 576, 2),
+		*[SandglassBlock(6, 576) for _ in range(3)],
+
+		SandglassBlock(6, 960),
+		SandglassBlock(6, 960),
+
+		SandglassBlock(6, 1280),
+
+		layers.Conv2D(1280, 7),
+		layers.BatchNormalization(-1),
+		act_to_use(act_arg1),
+
+		layers.Conv2D(10, 1),
+		layers.Flatten(),
+		layers.Activation('softmax') #Proper formatting for the thingy
+	])
+	
+	metrics_to_use = [
+        tf.keras.metrics.SparseTopKCategoricalAccuracy(k=5, name="T5"),
+        tf.keras.metrics.SparseTopKCategoricalAccuracy(k=3, name="T3"),
+        tf.keras.metrics.SparseTopKCategoricalAccuracy(k=1, name="T1")
+    ]
+
+	calls = [
+		tf.keras.callbacks.TerminateOnNaN(),
+		#tf.keras.callbacks.LearningRateScheduler(h.lr_schedule_creator(LRATE_SCHED, LRATE_RATIO))
+	]
+
+#neXt.summary()
+#neXt.layers[3].sequence.summary()
+
+print(f"\nStarting {TYPE} training for MobileNeXt")
+neXt.compile(optim, loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=metrics_to_use)
+hist = neXt.fit(train_ds, epochs=EPOCHS, validation_data=val_ds, callbacks=calls)
+
+h.output_training_history(logger, hist)
+h.output_validation_history(logger, hist)
